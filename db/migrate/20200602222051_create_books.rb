33e806a1200d428ab5904062e4fc0ccf81eb765c
Anthony <pardo.anthony@gmail.com>
2020-06-02 17:32:41 -0500

    generated some models

---
+++ db/migrate/20200602222051_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.integer :author_id
+      t.string :summary
+      t.string :image_url
+      t.integer :user_id
+      t.integer :genre_id
+
+      t.timestamps
+    end
+  end
+end
