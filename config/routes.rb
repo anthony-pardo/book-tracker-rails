bd24b1e1dc263a8f2e69020fcff384dd6d1021f4
Anthony <pardo.anthony@gmail.com>
2020-06-05 15:29:12 -0500

    created and updated omniauth_callbacks_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :genres
   devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
