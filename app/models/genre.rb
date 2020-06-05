14b920c993303472d6780bbc0bf5b596b59f3bc0
Anthony <pardo.anthony@gmail.com>
2020-06-04 20:30:25 -0500

    added model associations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+  has_many :books
+  has_many :users, through: :books 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :books
+  has_many :genres, through: :books 
+  has_many :reviews
+  has_many :comments
+         
 end
