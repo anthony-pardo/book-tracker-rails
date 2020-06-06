9c6b14294cb89fca0140ecaed1d750988963eec4
Anthony <pardo.anthony@gmail.com>
2020-06-06 17:08:04 -0500

    added new action to BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,4 +23,12 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
     end
   end
+
+  def new
+    if params[:user_id] && !User.exists?(params[:user_id])
+      redirect_to users_path, alert: "User not found."
+    else
+      @book = Book.new(user_id: params[:user_id])
+    end
+  end
 end
