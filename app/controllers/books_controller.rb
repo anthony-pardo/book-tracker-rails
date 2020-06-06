539546d5c8eaace365a7deb6dcf2e4c5d74c19c2
Anthony <pardo.anthony@gmail.com>
2020-06-05 20:13:25 -0500

    updated books_controller with index and show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,26 @@
 class BooksController < ApplicationController
+  def index
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      if @user.nil?
+        redirect_to users_path, alert: "User not found"
+      else
+        @books = @user.books
+      end
+    else
+      @books = Book.all
+    end
+  end
+
+  def show
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      @book = @user.books.find_by(id: params[:id])
+      if @book.nil?
+        redirect_to user_books_path(@user), alert: "Book not found"
+      end
+    else
+      @book = Book.find(params[:id])
+    end
+  end
 end
