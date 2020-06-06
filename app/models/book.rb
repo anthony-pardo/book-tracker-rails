862cdbd4df30b497c0bcbedbde74d2138438e58f
Anthony <pardo.anthony@gmail.com>
2020-06-06 17:19:03 -0500

    added user_name and user_name= to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,13 @@ class Book < ApplicationRecord
   has_many :genres
 
   validates :title, presence: true
+
+  def user_name
+    self.try(:user).try(:name)
+  end
+
+  def user_name=(name)
+    user = User.find_or_create_by(name: name)
+    self.user = user
+  end
 end
