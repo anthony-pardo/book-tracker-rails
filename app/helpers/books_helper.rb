cbe13d36102f3fb836de33a48bb0ccef1f338175
Anthony <pardo.anthony@gmail.com>
2020-06-06 17:04:12 -0500

    created user_select helper for books

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+  def user_select(book, isNested)
+    if !isNested
+      select_tag "book[user_id]", options_from_collection_for_select(User.all, :id, :name)
+    else
+      hidden_field_tag "book[user_id]", book.user_id
+    end
+  end
 end
