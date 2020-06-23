0bc698e3acf05750b749ff3713a959f38a18ce46
Anthony <pardo.anthony@gmail.com>
2020-06-22 19:26:56 -0500

    updated migrations without genres

--- db/migrate/20200602222051_create_books.rb
+++ db/migrate/20200602222051_create_books.rb
@@ -4,10 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :summary
       t.string :image_url
-      t.belongs_to :user, index: true
-      t.belongs_to :genre, index: true
-      #t.integer :user_id
-      #t.integer :genre_id
+
 
       t.timestamps
     end
